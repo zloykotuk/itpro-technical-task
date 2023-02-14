32c920b8805be7842d2525d04bab7392787980b2
Alexander Burgela <7loykotuk@gmail.com>
2023-02-14 23:52:06 +0200

    Installed vue js

---
+++ resources/js/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Welcome to Vue 3 JS</h1>
+</template>
+
+<script>
+export default {
+    name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    server: {
+        https: false,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                }
+            }
+        })
     ],
 });
